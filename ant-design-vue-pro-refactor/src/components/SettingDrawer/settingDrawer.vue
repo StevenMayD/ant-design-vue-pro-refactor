633a01e9e570e286937107f957243bc903344a86
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-02-24 14:23:54 +0800

    抽屉css样式到独立css文件

--- ant-design-vue-pro-refactor/src/components/SettingDrawer/settingDrawer.vue
+++ ant-design-vue-pro-refactor/src/components/SettingDrawer/settingDrawer.vue
@@ -9,7 +9,7 @@
   >
     <!-- 设置抽屉直接挂载到DOM上 通过handle控制抽屉的打开关闭 -->
     <template v-slot:handle>
-      <div class="handle" @click="visible = !visible">
+      <div class="setting-drawer-handle" @click="visible = !visible">
         <!-- 组件的方式使用图标 ( <a-icon>的方式 使用出错 )-->
         <apple-outlined v-if="!visible" />
         <android-outlined v-if="visible" />
@@ -48,18 +48,5 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
-.handle {
-  position: absolute;
-  top: 240px;
-  right: 255px;
-  width: 48px;
-  height: 48px;
-  background: #1890ff;
-  color: #fff;
-  font-size: 20px;
-  text-align: center;
-  line-height: 48px;
-  border-radius: 3px 0 0 3px;
-}
-</style>
+<!-- 将scope样式 抽离到独立的index.less文件中 -->
+<style lang="less" src="./index.less"></style>
---
+++ ant-design-vue-pro-refactor/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        // 这里的选项会传递给 css-loader
+        modifyVars: {
+          "primary-color": "#1DA57A", // 全局生效的主题颜色配置
+        },
+        javascriptEnabled: true, //允许链式调用的换行
+      },
+    },
+  },
+};
