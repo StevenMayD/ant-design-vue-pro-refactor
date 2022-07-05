ae109f979de2b658b23670f46e36a4787e79b61d
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-07-05 15:10:57 +0800

    国际化补充描述

--- ant-design-vue-pro-refactor/src/App.vue
+++ ant-design-vue-pro-refactor/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <!-- 使用国际化组件 将所有页面内容包裹，参数传递locale属性 -->
+    <!-- 国际化1：ConfigProvider、多数组件的国际化（使用国际化组件 将所有页面内容包裹，参数传递locale属性） -->
     <a-config-provider :locale="locale">
       <!-- 路由占位符：用于加载主体页面 -->
       <router-view />
@@ -24,7 +24,7 @@ export default {
     // 监听路由上的query上的locale参数，并赋值给locale变量
     "$route.query.locale": function (val) {
       this.locale = val === "enUS" ? enUS : zhCN;
-      moment.locale(val === "enUS" ? "en" : "zh-cn");
+      moment.locale(val === "enUS" ? "en" : "zh-cn"); // 国际化2：moment、时间类型组件<a-date-picker>的国际化
     },
   },
 };
--- ant-design-vue-pro-refactor/src/main.js
+++ ant-design-vue-pro-refactor/src/main.js
@@ -43,9 +43,10 @@ import {
 //   },
 // };
 
+// 国际化3：vue-i18n、自定义组件与自定义文案的国际化
 // vue-i18n的两个配置属性：locale、messages
 const i18n = new createI18n({
-  // 从url中取locale参数 需要用到一个三方库解析url
+  // 从url中取locale参数 需要用到一个三方库解析url：将location.search字符串解析成对象，取locale值
   locale: queryString.parse(location.search).locale || "zhCN", // 添加默认值
   // 配置语言包（注意是messages 不是message）
   // 方式一：使用封装的独立文件
